06f8e646869f03d692829c75081e8182093397aa
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2022-09-17 21:37:44 +0530

    Added Extract Account Job to main app

--- agenda/jobs/extractAccounts.js
+++ agenda/jobs/extractAccounts.js
@@ -1,5 +1,4 @@
-const Axios = require('axios');
-const { Donation } = require('../../models');
+const { Donation, Account } = require('../../models');
 const agenda = require('../index');
 const jobTypes = require('../jobType');
 
@@ -16,21 +15,92 @@ agenda.define(jobTypes.EXTRACT_ACCOUNTS, async (job, done) => {
   }
 });
 
-async function run() {
+async function extractAccountsFromDonation(criteria, offset, limit) {
     const query = [
         {
-            $match: {},
+            $match: criteria,
+        },
+        {
+            $skip: offset,
+        },
+        {
+            $limit: limit,
         },
         {
             $group: {
                 _id: '$from',
                 active_chains: {
-                    $push: '$chain',
+                    $addToSet: '$chain',
+                },
+                donations: {
+                    $push: {
+                        txn_hash: '$txn_hash',
+                        amount: '$amount',
+                        token: '$token',
+                        chain: '$chain'
+                    },
                 },
             },
+        },
+        {
+            $project: {
+                _id: false,
+                address: '$_id',
+                active_chains: 1,
+                donations: 1,
+            }
         }
     ];
-    const acounts = await Donation.aggregate(query);
-    console.log(acounts);
+    const extractedAcounts = await Donation.aggregate(query);
+    return extractedAcounts;
+}
+
+async function addDataToAccount(accountData) {
+    let foundAccount = await Account.findOne({ main_address: accountData.address });
+    if (!foundAccount) {
+        foundAccount = new Account({ main_address: accountData.address });
+    }
+    let old_active_chains = foundAccount && foundAccount.active_chains || [];
+    let new_active_chains = accountData && accountData.active_chains || [];
+    const chainMap = {};
+    old_active_chains.forEach(elem => {
+        chainMap[elem] = 1;
+    });
+    new_active_chains.forEach(elem => {
+        chainMap[elem] = 1;
+    });
+    foundAccount.active_chains = Object.keys(chainMap);
+
+    const old_donations = foundAccount && foundAccount.donations || [];
+    const new_donations = accountData && accountData.donations || [];
+    const donationMap = {};
+    old_donations.forEach(elem => {
+        donationMap[elem.txn_hash] = elem;
+    });
+    new_donations.forEach(elem => {
+        donationMap[elem.txn_hash] = elem;
+    });
+    foundAccount.donations = Object.values(donationMap);
+    await foundAccount.save();
+}
+
+async function processAccounts(accounts) {
+    const allPromises = accounts.map(async (elem) => {
+        await addDataToAccount(elem);
+    });
+    const data = await Promise.all(allPromises);
+    console.log(data);
+}
+
+async function run() {
+    const criteria = {};
+    const totalDonations = await Donation.find(criteria).count();
+    const limit = 100;
+    const batches = Math.ceil(totalDonations / limit);
+    for (let i = 0; i < batches; i++) {
+        const offset = i * limit;
+        const extractedAccounts = await extractAccountsFromDonation(criteria, offset, limit);
+        await processAccounts(extractedAccounts);
+    }
     console.log('Success!');
 }
