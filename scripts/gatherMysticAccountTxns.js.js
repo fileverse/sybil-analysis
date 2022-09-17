3e8810f99b0559c6612d73018468dd76d099be85
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2022-09-18 03:10:40 +0530

    Added Mystic Account Txn Gathering Script

---
+++ scripts/gatherMysticAccountTxns.js.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-process-exit */
+require('../utils/database');
+const agenda = require('../agenda');
+const jobTypes = require('../agenda/jobType');
+const { Account } = require('../models');
+
+async function getAccount(criteria, offset, limit) {
+  const accounts = await Account.find(criteria).skip(offset).limit(limit).lean();
+  return accounts[0];
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+class Script {
+  static async run() {
+    const criteria = { in_gitcoin_list: { $ne: true } };
+    const totalAccounts = await Account.find(criteria).count();
+    for (let i = 0; i < totalAccounts; i++) {
+        console.log("account number: ", i, " out of ", totalAccounts);
+        console.log('percentage done: ', parseInt((i / totalAccounts) * 100, 10));
+        const currentAccount = await getAccount(criteria, i, 1);
+        console.log(currentAccount);
+        agenda.schedule('now', jobTypes.GET_ZKSYNC_TXNS, {
+          address: currentAccount.main_address,
+          offset: 0,
+          limit: 100,
+        });
+        await sleep(120000);
+    }
+  }
+}
+
+(async () => {
+  try {
+    console.log('Script run started!');
+    await Script.run();
+    console.log('Script run complete!');
+    process.exit(0);
+  } catch (err) {
+    console.error(err.stack);
+    process.exit(1);
+  }
+})();
+
